2b720476b4f93895caa77e3d8e54fe31d3a781e6
Dusty-Meg <matthewemmoore@googlemail.com>
2014-03-19 10:45:00 +0000

    Unity Prototype
    Catapult prototype in unity

---
+++ Disgruntled Whales/Assets/right.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class right : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 move = new Vector3 (-4, 3, 1.5f);
+		Quaternion rot = new Quaternion (0, 0, 0, 0);
+		transform.position = move;
+		transform.rotation = rot;
+	}
+}
---
+++ Disgruntled Whales/Assets/test.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	bool mouse = false;
+	float x, y;
+	
+	void OnMouseDown()
+	{
+		Debug.Log("Pressed left click.");
+		mouse = true;
+		x = Input.mousePosition.x;
+		y = Input.mousePosition.y;
+	}
+
+	void OnMouseUp()
+	{
+		mouse = false;
+
+		SpringJoint2D[] tester = GetComponents<SpringJoint2D> ();
+
+		foreach(SpringJoint2D testr in tester)
+		{
+
+			testr.enabled = false;
+		}
+	}
+
+	void Update()
+	{
+		if (mouse == true)
+		{
+			Debug.Log(Input.mousePosition);
+
+			Vector2 move2 = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+
+			transform.position = (move2);
+
+			Quaternion rot = new Quaternion (0, 0, 0, 0);
+			transform.rotation = rot;
+		}
+	}
+
+}
